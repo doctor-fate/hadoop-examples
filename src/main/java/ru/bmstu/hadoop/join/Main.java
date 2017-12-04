b4a54be6c589f3d75ef01a8a4cb3499df23211e3
doctor-fate <stahma.tarr@outlook.com>
2017-12-04 16:54:06 +0300

    Inheriting from Configured in L2.

--- src/main/java/ru/bmstu/hadoop/join/Main.java
+++ src/main/java/ru/bmstu/hadoop/join/Main.java
@@ -1,6 +1,6 @@
 package ru.bmstu.hadoop.join;
 
-import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -10,16 +10,14 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-public class Main implements Tool {
-    private Configuration configuration;
-
+public class Main extends Configured implements Tool {
     public static void main(String[] args) throws Exception {
         int status = ToolRunner.run(null, new Main(), args);
         System.exit(status);
     }
 
     public int run(String[] args) throws Exception {
-        Job job = Job.getInstance(configuration);
+        Job job = Job.getInstance(getConf());
         job.setJarByClass(Main.class);
 
         job.setReducerClass(JoinReducer.class);
@@ -38,13 +36,5 @@ public class Main implements Tool {
         job.setNumReduceTasks(2);
 
         return job.waitForCompletion(true) ? 1 : 0;
-    }
-
-    public Configuration getConf() {
-        return configuration;
-    }
-
-    public void setConf(Configuration configuration) {
-        this.configuration = configuration;
     }
 }
